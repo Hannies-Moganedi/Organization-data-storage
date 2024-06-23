6502f5d67322c1fb6d134eba7cbf44041be36763
Hannies-Moganedi <164213753+Hannies-Moganedi@users.noreply.github.com>
2024-06-23 10:45:29 +0200

    Update classes.php

--- includes/classes.php
+++ includes/classes.php
@@ -1,1 +1,37 @@
+<?php
+//function to sanitize data
+function sanitizeInput($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $code = sanitizeInput($_POST['code']);
+    $subject = sanitizeInput($_POST['subject']);
+    $teacher = sanitizeInput($_POST['teacher']);
+    $location = sanitizeInput($_POST['location']);
+    $textbook = sanitizeInput($_POST['textbook']);
+    // Validate form data
+    if (empty($code) || empty($subject) || empty($teacher) || empty($location) || empty($textbook)) {
+        echo "All fields are required";
+    } else {
+        require '../dbh.inc.php';
 
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "INSERT INTO classes (CODE, SUBJECT, TEACHER, LOCATION, TEXTBOOK) VALUES ('$code', '$subject', '$teacher', '$location', '$textbook')";
+            
+        if ($conn->query($sql) === TRUE) {
+            header("Location: ../classes.php?record=success");
+            exit();
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        //Close database connection
+        $conn->close(); 
+    }
+}
+?>
